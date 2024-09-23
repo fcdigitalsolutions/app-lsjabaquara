fb7ef888d026a4a0ee68b5a5ee52a02e77bd329c
fabio cardoso <fbc.fabiocardoso@gmail.com>
2024-09-22 22:59:57 -0300

    atualizar o dominio da api

--- src/pages/LoginForm.jsx
+++ src/pages/LoginForm.jsx
@@ -14,7 +14,7 @@ const LoginForm = () => {
         e.preventDefault();
 
         try {
-            const response = await axios.post('http://137.184.190.156:5000/auth/login', {
+            const response = await axios.post('http://ls-jabaquara.com.br/auth/login', {
                 user_login: username,
                 user_pswd: password,
             });
